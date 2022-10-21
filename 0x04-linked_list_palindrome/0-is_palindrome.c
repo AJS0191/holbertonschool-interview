4af1b437cda0737950b1afbf52f80f182b006209
AJS0191 <ashtonjamess@live.com>
2022-10-21 10:17:10 -0500

    remove

--- 0x04-linked_list_palindrome/0-is_palindrome.c
+++ 0x04-linked_list_palindrome/0-is_palindrome.c
@@ -1,30 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
 
-/**
- * add_node - adds a node to a linked list
- *
- * @head: linked list being added too
- * @str: string being inserted in node
- *
- * Return: returns the address of the updated list
- **/
-
-listint_t *add_node(listint_t **head, int n)
-{
-	listint_t *newn;
-
-	newn = malloc(sizeof(listint_t));
-	if (!newn)
-	{
-		return (NULL);
-	}
-	newn->next = *head;
-	newn->n = n;
-	*head = newn;
-
-	return (*head);
-}
 
 
 int is_palindrome(listint_t **head)
